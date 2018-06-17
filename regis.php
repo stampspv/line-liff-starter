67a13fe578e80ecb3eb5af1cf7ff41a7e216ee54
stampspv <suphavich.c@gmail.com>
2018-06-17 12:01:50 +0700

    5.10

--- regis.php
+++ regis.php
@@ -9,10 +9,22 @@ if(isset($_GET['name'])){
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
-
+	mysqli_set_charset($conn,"utf8");
 	// Check connection
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
+	}else{
+		$name = $_GET['name'];
+		$email = $_GET['email'];
+		$telephone = $_GET['tel'];
+		$userid = $_GET['userid'];
+		$sql = "INSERT INTO `customers` VALUES ('','$name','$email','$telephone','$userid','0','1','','')";
+
+		if ($conn->query($sql) === TRUE) {
+		    echo "New record created successfully";
+		} else {
+		    echo "Error: " . $sql . "<br>" . $conn->error;
+		}
 	}
 }else{
 
@@ -24,12 +36,16 @@ if(isset($_GET['name'])){
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <script src="//code.jquery.com/jquery.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
+<head>
+	<title>ลงทะเบียน Dutchmill</title>
+  	<meta charset="UTF-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <body>
 	<div class="row">
 		<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 		<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
-			<center><h1>UPPERCUZ DEV 5.9</h1></center>
+			<center><h1>UPPERCUZ DEV 5.10</h1></center>
 			<form action="/regis.php" method="get">
 			  <div class="form-group">
 			    <label>ชื่อ-นามสกุล :</label>
