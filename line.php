86411d40b6073f5787dbf39aa5c9343525c0eb2a
stampspv <suphavich.c@gmail.com>
2018-06-17 22:54:27 +0700

    5.15

--- line.php
+++ line.php
@@ -5,7 +5,7 @@ $api_url = 'https://api.line.me/v2/bot/user/U216566c778cf0a50ae0b8e87cd71fe04/li
 $accessToken = 'HckYQU7o6y6Fbtm886GA4qLdAaO3Ik3Ho657LkWiNvC+J4Xa+YBWOZhPDGR2fhvZihfOrlPAosEfgQdwOF3DQHd5Jhc6GWFALc5o9FC/zDLPWN5+UrSwqhNxwgquDAfuxnENMA/US/jFpQy4l/NB8QdB04t89/1O/w1cDnyilFU=';
 
 $v = Array(
-    'Authorization: Bearer'.$accessToken
+    'Authorization: Bearer '.$accessToken
 );
 
 $ch = curl_init();
